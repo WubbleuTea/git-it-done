5e7a93e6e34daf0822c8190a08f789b49300e68f
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-06-23 17:43:39 -0500

    repos with a feature can be pulled form console

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -75,8 +75,16 @@ var getUserRepos = function(user) {
 var getFeaturedRepos = function(language) {
     var apiUrl = "https://api.github.com/search/repositories?q=" + language + "is:featured&sort=help-wanted-issues";
 
-    fetch(apiUrl);
-}
+    fetch(apiUrl).then(function(response){
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: " + response.statusText)
+        }
+    });
+};
 
 
 //handles click of username submission
